9ea10c52d14d879ebbbbb25335c10d06c702a3a6
tsu <smthanh@gmail.com>
2025-03-11 00:37:12 +0700

    Update install.sh

--- docker-compose/withPostgresAndWorker/install.sh
+++ docker-compose/withPostgresAndWorker/install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 echo "--------- 🟢 Start install docker -----------"
-apt-get remove docker docker-engine docker.io containerd runc
-apt-get install ca-certificates curl gnupg lsb-release
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
 apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 # echo "--------- 🔴 Finish install docker -----------"
 
@@ -15,19 +22,6 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 # sudo chown -R 1000:1000 vol_n8n
 # sudo chmod -R 755 vol_n8n
 # echo "--------- 🔴 Finish creating folder -----------"
-
-DOCKER_VERSION="27.0"
-
-curl -s https://releases.rancher.com/install-docker/${DOCKER_VERSION}.sh | sh 2>&1
-if ! [ -x "$(command -v docker)" ]; then
-    curl -s https://get.docker.com | sh -s -- --version ${DOCKER_VERSION} 2>&1
-    if ! [ -x "$(command -v docker)" ]; then
-        echo " - Docker installation failed."
-        echo "   Maybe your OS is not supported?"
-        echo " - Please visit https://docs.docker.com/engine/install/ and install Docker manually to continue."
-        exit 1
-    fi
-fi
 
 echo "--------- 🟢 Start docker compose up  -----------"
 wget https://raw.githubusercontent.com/suminhthanh/n8n-hosting/refs/heads/main/docker-compose/withPostgresAndWorker/.env -O .env
