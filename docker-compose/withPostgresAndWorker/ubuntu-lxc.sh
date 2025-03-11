1469e65c51a881a0aacc80d9aef111e4812f719e
tsu <smthanh@gmail.com>
2025-03-11 23:13:28 +0700

    update

--- docker-compose/withPostgresAndWorker/ubuntu-lxc.sh
+++ docker-compose/withPostgresAndWorker/ubuntu-lxc.sh
@@ -19,46 +19,16 @@ variables
 color
 catch_errors
 
-function install_docker() {
-  echo "--------- 🟢 Start install docker -----------"
-  apt-get update -y
-  apt-get install -y ca-certificates curl
-  install -m 0755 -d /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  chmod a+r /etc/apt/keyrings/docker.asc
-
-  echo \  
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \  
-    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \  
-    tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-  apt-get update -y
-  apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-  echo "--------- 🔴 Finish install docker -----------"
-}
-
-function setup_n8n() {
-  echo "--------- 🟢 Start docker compose up  -----------"
-  wget https://raw.githubusercontent.com/suminhthanh/n8n-hosting/refs/heads/main/docker-compose/withPostgresAndWorker/.env -O .env
-  wget https://raw.githubusercontent.com/suminhthanh/n8n-hosting/refs/heads/main/docker-compose/withPostgresAndWorker/docker-compose.yml -O compose.yaml
-
-  export INTERNAL_IP=$(hostname -I | cut -f1 -d' ')
-  export EXTERNAL_IP=https://$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 6 | head -n 1).n8nhosting.app
-  echo "Using domain: $EXTERNAL_IP"
-
-  curl --location "https://n8n-auto.vnict.workers.dev/" \
-  --header "Content-Type: application/json" \
-  --data "{\"service\": \"http://$INTERNAL_IP:5678\",\"hostname\": \"$EXTERNAL_IP\"}"
-
-  -E docker compose up -d
-  echo "--------- 🔴 Finish! Wait a few minutes and test in browser at url $EXTERNAL_IP for n8n UI FROM INTERNAL_IP=$INTERNAL_IP -----------"
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  $STD curl -fsSL https://raw.githubusercontent.com/suminhthanh/n8n-hosting/main/docker-compose/withPostgresAndWorker/install.sh | bash
 }
 
 start
 build_container
 description
-install_docker
-setup_n8n
 
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
