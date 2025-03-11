ab118d256ca7c6cc459bce0bd68d4b8daa4f53ef
tsu <smthanh@gmail.com>
2025-03-12 00:33:14 +0700

    sleep 20

--- docker-compose/withPostgresAndWorker/lxc.sh
+++ docker-compose/withPostgresAndWorker/lxc.sh
@@ -11,15 +11,7 @@ pct start $NEW_ID
 
 # Chờ container khởi động hoàn toàn
 echo "Đang chờ LXC $NEW_ID khởi động..."
-while ! pct exec $NEW_ID -- bash -c "echo Container Ready" &>/dev/null; do
-    sleep 2
-done
-
-# Chờ mạng sẵn sàng
-echo "Đang chờ mạng LXC $NEW_ID sẵn sàng..."
-while ! pct exec $NEW_ID -- ping -c 1 8.8.8.8 &>/dev/null; do
-    sleep 2
-done
+sleep 20
 
 # Chạy script domain.sh bên trong container
 pct exec $NEW_ID -- bash -c "curl -fsSL https://raw.githubusercontent.com/suminhthanh/n8n-hosting/main/docker-compose/withPostgresAndWorker/domain.sh | bash"
