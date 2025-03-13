4b0507b63831aeb9dcf92580ed0c22795f7dc799
tsu <smthanh@gmail.com>
2025-03-13 12:46:49 +0700

    stop!!!

--- docker-compose/withPostgresAndWorker/lxc.sh
+++ docker-compose/withPostgresAndWorker/lxc.sh
@@ -3,25 +3,27 @@
 SOURCE_ID=$1
 echo "Source ID: $SOURCE_ID"
 
-# Lấy ID tiếp theo tự động từ Proxmox
-NEW_ID=$(pvesh get /cluster/nextid)
+exit 0
 
-echo "Sẽ tạo LXC mới với ID: $NEW_ID"
+# # Lấy ID tiếp theo tự động từ Proxmox
+# NEW_ID=$(pvesh get /cluster/nextid)
 
-# Clone LXC container
-pct clone $SOURCE_ID $NEW_ID --hostname n8n --full
-pct start $NEW_ID
+# echo "Sẽ tạo LXC mới với ID: $NEW_ID"
 
-# Chờ container khởi động hoàn toàn
-echo "Đang chờ LXC $NEW_ID khởi động..."
-while ! pct exec $NEW_ID -- systemctl is-system-running --quiet; do
-    echo "Đang chờ container khởi động..."
-    sleep 5
-done
-sleep 5
+# # Clone LXC container
+# pct clone $SOURCE_ID $NEW_ID --hostname n8n --full
+# pct start $NEW_ID
 
-# Chạy script domain.sh bên trong container
-echo "Chạy script domain.sh bên trong container..."
-pct exec $NEW_ID -- bash -c "curl -fsSL -o- https://raw.githubusercontent.com/suminhthanh/n8n-hosting/main/docker-compose/withPostgresAndWorker/domain.sh | bash -s -- $NEW_ID"
-echo "LXC $NEW_ID đã sẵn sàng!"
-sleep 2
+# # Chờ container khởi động hoàn toàn
+# echo "Đang chờ LXC $NEW_ID khởi động..."
+# while ! pct exec $NEW_ID -- systemctl is-system-running --quiet; do
+#     echo "Đang chờ container khởi động..."
+#     sleep 5
+# done
+# sleep 5
+
+# # Chạy script domain.sh bên trong container
+# echo "Chạy script domain.sh bên trong container..."
+# pct exec $NEW_ID -- bash -c "curl -fsSL -o- https://raw.githubusercontent.com/suminhthanh/n8n-hosting/main/docker-compose/withPostgresAndWorker/domain.sh | bash -s -- $NEW_ID"
+# echo "LXC $NEW_ID đã sẵn sàng!"
+# sleep 2
